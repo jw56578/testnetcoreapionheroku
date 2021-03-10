ed92f99c172b626e71902c9287b2d7f41ff6b183
Jon Woo <jw56578@gmail.com>
2021-03-09 18:07:04 -0600

    trying to get it to work

--- readme.md
+++ readme.md
@@ -2,3 +2,11 @@
 * using a heroku build back
 * what is the least we need to do, so the buildpack works?
 * will adding the app.json do it?
+  * NO - this didn't work
+All we need to do is set the buildpack to the url in the Settings
+how do you set the port to listen on?
+    * going to the heroku app url is getting 404
+    * the example project works so we are missing something
+
+
+Just use the project example that exists and remove things until it stops working du
--- testserver/Startup.cs
+++ testserver/Startup.cs
@@ -1,51 +1,53 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace testserver
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services
+                .AddHttpsRedirection(options => { options.HttpsPort = 443; })
+                .AddMvcCore()
+                .AddCors(options =>
+                {
+                    options.AddPolicy("CorsPolicy",
+                        builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+                });
+
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor |
+                                           ForwardedHeaders.XForwardedProto;
+                options.KnownNetworks.Clear();
+                options.KnownProxies.Clear();
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
+            app.UseForwardedHeaders();
+
+            if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("DYNO")))
             {
-                app.UseDeveloperExceptionPage();
+                Console.WriteLine("Use https redirection");
+                app.UseHttpsRedirection();
             }
 
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app
+                .UseRouting()
+                .UseDefaultFiles()
+                .UseStaticFiles()
+                .UseCors("CorsPolicy")
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapDefaultControllerRoute();
+                });
         }
     }
 }
